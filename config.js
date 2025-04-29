f2858d7875b6a3c1bd123e2facb59baa2801fd54
dada19960509 <greatdada@qq.com>
2025-04-29 10:58:25 +0800

    Update config.js

--- config.js
+++ config.js
@@ -29,6 +29,8 @@ var config = {
      */
     imgs: {
         "心爱的小熊熊": "./imgs/xiaokeai1.png",
+        "我要你岁岁有我！": "./imgs/wo.png",
+        "这是我们在一起的": "./imgs/yiqi.png",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
